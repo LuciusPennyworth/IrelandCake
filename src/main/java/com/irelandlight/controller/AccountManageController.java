aff3ddfd14b0584c5a07466ca631085bdbc31599
Matt <522389629@qq.com>
2016-12-25 20:09:00 +0800

    Signed-off-by: Matt <522389629@qq.com>

--- src/main/java/com/irelandlight/controller/AccountManageController.java
+++ src/main/java/com/irelandlight/controller/AccountManageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.annotation.Resource;
 import java.util.Map;
 
